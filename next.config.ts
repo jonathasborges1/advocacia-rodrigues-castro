8fc1411ccd158daf531180248795c6115a1c89ca
Jonathas Borges <jbc@icomp.ufam.edu.br>
2025-02-25 01:23:17 -0400

    deploy cloudflare #3

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export", // Necessário para Cloudflare Pages
+  distDir: "out", // Define o diretório de saída para "out"
   trailingSlash: true, // Adiciona barra no final das URLs, compatível com export estático
   images: {
     unoptimized: true, // Evita otimização de imagens no Cloudflare Pages
