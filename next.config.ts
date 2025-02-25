62f0a2b8e842af79ff5a8f856a97ac4f0c75680a
Jonathas Borges <jbc@icomp.ufam.edu.br>
2025-02-25 01:02:49 -0400

    deploy cloudflare #1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Necessário para Cloudflare Pages
+  images: {
+    unoptimized: true, // Evita otimização de imagens no Cloudflare Pages
+  },
 };
 
 export default nextConfig;
